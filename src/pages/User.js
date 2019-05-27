03dcc9e73bbc9140b2cc8b1cf2d5839dd720f46c
AdamLBT <957663046@qq.com>
2019-05-27 21:59:39 +0800

    KFC后台管理系统Demo

---
+++ src/pages/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class User extends React.Component {
+
+  render(){
+    return (
+      <div className="user">
+        <h2>教师管理</h2>
+      </div>
+    )
+  }
+}
+
+export default User;
